7f9997b2906471090d001a16878c8a00024fd546
ChaeHaejoong <146797747+ChaeHaejoong@users.noreply.github.com>
2024-08-01 15:52:43 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+import { numberToText } from "./util.js";
+
+const whiteSpace = document.querySelector(".whiteSpace");
+const whiteRemainingTime = document.querySelector(".whiteRemainingTime")
+const blackSpace = document.querySelector(".blackSpace");
+const blackRemainingTime = document.querySelector(".blackRemainingTime")
+
+let min = prompt("몇 분간 진행할 것인지 입력");
+
+let isGameOver = false;
+let whiteTimer;
+let blackTimer;
+let isWhiteTurn = true;
+let isBlackTurn = false;
+let whiteTimeLeft = min * 60; // 900 sec
+let blackTimeLeft = min * 60;
+
+whiteRemainingTime.innerHTML = numberToText(whiteTimeLeft);
+blackRemainingTime.innerHTML = numberToText(blackTimeLeft);
+
+function gameOver() {
+  clearInterval(whiteTimer);
+  clearInterval(blackTimer);
+
+  isGameOver = true;
+}
+
+function turnChange() {
+  isWhiteTurn = !isWhiteTurn;
+  isBlackTurn = !isBlackTurn;
+  
+  if (whiteTimeLeft <= 30) {
+    whiteTimeLeft = 30;
+    whiteRemainingTime.innerHTML = numberToText(whiteTimeLeft);
+  }
+  if (blackTimeLeft <= 30) {
+    blackTimeLeft = 30;
+    blackRemainingTime.innerHTML = numberToText(blackTimeLeft)
+  }
+}
+
+function timeAlert() {
+  if(whiteTimeLeft <= 30) {
+    whiteRemainingTime.style.color = "tomato";
+  }
+
+  if(blackTimeLeft <= 30) {
+    blackRemainingTime.style.color = "tomato";
+  }
+}
+
+whiteSpace.addEventListener("click", () => {
+
+  if (!isWhiteTurn) return;
+
+  if (whiteTimeLeft <= 30) {
+    whiteTimeLeft = 30;
+  }
+  
+  clearInterval(whiteTimer);
+  turnChange();
+  blackTimer = setInterval(() => {
+    blackTimeLeft--;
+    if(blackTimeLeft == 0) gameOver();
+    timeAlert();
+    blackRemainingTime.innerHTML = numberToText(blackTimeLeft);
+  }, 1000)
+})
+
+
+blackSpace.addEventListener("click", () => {
+  
+  if (!isBlackTurn) return;
+  
+  clearInterval(blackTimer);
+  turnChange();
+  whiteTimer = setInterval(() => {
+    whiteTimeLeft--;
+    if(whiteTimeLeft == 0) gameOver();
+    timeAlert();
+    whiteRemainingTime.innerHTML = numberToText(whiteTimeLeft);
+  }, 1000)
+})
